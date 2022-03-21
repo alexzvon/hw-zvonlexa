d22b98a4564c0a6bcee03ecfe453fadf1bd59310
zvonlexa <zvon.lexa@yandex.ru>
2022-03-21 20:28:04 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,71 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+)
+
+func Top10(sss string) []string {
+	asm := make(map[string]int)
+
+	if sss == "" {
+		return []string{}
+	}
+
+	type ams struct {
+		key   string
+		value int
+	}
+	var sam []ams
+	var out []string
+
+	re := regexp.MustCompile(`\s+`)
+	sm := re.Split(sss, -1)
+
+	for i := 0; i < len(sm)-1; i++ {
+		asm[sm[i]]++
+	}
+
+	for k, v := range asm {
+		sam = append(sam, ams{k, v})
+	}
+
+	sort.Slice(sam, func(i, j int) bool {
+		return sam[i].value > sam[j].value
+	})
+
+	var val, count, start, end int
+
+	for i := 0; i < 10; i++ {
+		if val == 0 {
+			val = sam[i].value
+			start = i
+		}
+
+		if val != sam[i].value {
+			end = i
+			val = sam[i].value
+
+			if count > 1 {
+				sort.Slice(out[start:end], func(i, j int) bool {
+					return out[i+start] < out[j+start]
+				})
+			}
+
+			start = i
+			count = 0
+		}
+
+		count++
+		out = append(out, sam[i].key)
+	}
+
+	if count > 1 {
+		end = 10
+		sort.Slice(out[start:end], func(i, j int) bool {
+			return out[i+start] < out[j+start]
+		})
+	}
+
+	return out
 }
