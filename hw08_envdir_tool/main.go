178d0e7a6968d6fc9f4f4d743dfe61e3cedaadc3
zvonlexa <zvon.lexa@yandex.ru>
2022-05-27 15:12:36 +0300

    HW8 is completed

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		log.Fatalln("Недостаточно аргументов")
+	}
+
+	env, err := ReadDir(os.Args[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	os.Exit(RunCmd(os.Args[2:], env))
 }
