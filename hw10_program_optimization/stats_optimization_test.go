9ffdb77c40002d719459c6457a45f72e53ff3d87
zvonlexa <zvon.lexa@yandex.ru>
2022-08-03 13:29:36 +0300

    del all comments

--- hw10_program_optimization/stats_optimization_test.go
+++ hw10_program_optimization/stats_optimization_test.go
@@ -16,7 +16,6 @@ const (
 	timeLimit = 300 * time.Millisecond
 )
 
-// go test -v -count=1 -timeout=30s -tags bench .
 func TestGetDomainStat_Time_And_Memory(t *testing.T) {
 	bench := func(b *testing.B) {
 		b.Helper()
